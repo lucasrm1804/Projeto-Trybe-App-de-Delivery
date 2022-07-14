fb806d092a703aa4b111fdb21cbae8bc98ceefbd
JoseToitio <josetoitio@gmail.com>
2022-07-14 19:18:16 -0300

    Rota para pagina login feito

---
+++ front-end/src/routes/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from '../pages/Login';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/login" component={ Login } />
+      <Route exact path="/">
+        { <Redirect to="/login" /> }
+      </Route>
+    </Switch>
+  );
+}
