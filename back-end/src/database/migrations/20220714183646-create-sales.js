cc52af86df333b60e488a28652d2aa74503dc92a
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-14 15:46:20 -0300

    chore: create sales & sales models

---
+++ back-end/src/database/migrations/20220714183646-create-sales.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+      },
+      seller_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+      },
+      total_price: {
+        type: Sequelize.NUMBER
+      },
+      delivery_address: {
+        type: Sequelize.STRING
+      },
+      delivery_number: {
+        type: Sequelize.STRING
+      },
+      sale_date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      // createdAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // },
+      // updatedAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sales');
+  }
+};
---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Sales extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Sales.init({
+    total_price: DataTypes.NUMBER,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Sales',
+  });
+  return Sales;
+};
