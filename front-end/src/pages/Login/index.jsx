bd1c996c2267b2f2a02a5979888279f736351b2a
JoseToitio <josetoitio@gmail.com>
2022-07-15 16:39:57 -0300

    form login

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 export default function Login() {
   return (
     <div>
-      <h1>Login!</h1>
+      <form>
+        <label htmlFor="email">
+          {' '}
+          Login
+          <input
+            type="email"
+            name="email"
+            id="email"
+            data-testid="common_login__input-email"
+          />
+        </label>
+        <label htmlFor="password">
+          {' '}
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            data-testid="common_login__input-password"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="common_login__button-login"
+        >
+          LOGIN
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
     </div>
   );
 }
