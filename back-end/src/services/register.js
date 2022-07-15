270c614d2c1849f067850025ec98901d0f12e48a
murilo <murilocarrijoadm@gmail.com>
2022-07-15 18:56:15 -0300

    fix: update node

--- back-end/src/services/register.js
+++ back-end/src/services/register.js
@@ -1,8 +1,14 @@
+const crypto = require('crypto');
 const models = require('../database/models');
 
-const registerUser = async ({ name, email, password, role }) => {
+const registerUser = async ({ name, email, password }) => {
   try {
-    const newUser = await models.User.create({ name, email, password, role });
+    const getByEmail = await models.User.findOne({ where: { email } });
+    if (getByEmail) return null;
+    const passwordCrypto = crypto.createHash('md5').update(password).digest('hex');
+    const newUser = await models.User.create({
+      name, email, password: passwordCrypto, role: 'customer',
+    });
     return newUser;  
   } catch (error) {
     console.error(error);    
