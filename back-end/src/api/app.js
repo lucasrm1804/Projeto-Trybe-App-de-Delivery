5db80e47940c4b69b02e4e9676fd2c4abbb256a5
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-18 18:28:41 -0300

    chore: adds cors, and update register middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const register = require('../routes/register');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/', express.json());
 
