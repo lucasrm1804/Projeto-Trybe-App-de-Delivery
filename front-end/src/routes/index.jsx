f71274731d1bad2dfb0dfccc64f4241b30dc0d22
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-18 17:20:51 -0300

    chore: adds route to /register

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from '../pages/Login';
+import Register from '../pages/Login/Register';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/login" component={ Login } />
+      <Route path="/register" component={ Register } />
       <Route exact path="/">
         <Redirect to="/login" />
       </Route>
