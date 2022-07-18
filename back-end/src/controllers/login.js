fc2a296870b352e65c82e15b44c46f73c4bb07a2
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2022-07-18 18:12:00 -0300

    fix(login): add error 404 - not found

--- back-end/src/controllers/login.js
+++ back-end/src/controllers/login.js
@@ -2,12 +2,16 @@ const loginUser = require('../services/login');
 const token = require('../middleware/login.JWT');
 
 const OK = 200;
+const ERROR = 400;
 const INTERNAL_ERROR = 500;
 
 const loginController = async (req, res) => {
     try {
         const result = await loginUser(req.body);
         const { email, name, role } = result;
+        if(!result) {
+            return res.status(ERROR).json({ message: 'Not found' });
+        }
         return res.status(OK).json({
             name,
             email,
