9e3799f9c9d8355da6ab988a7644f16b52cc3c06
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-18 17:21:45 -0300

    chore: remove buttonForm component to normal button, and creates onCLick function

--- front-end/src/components/FormLogin/index.jsx
+++ front-end/src/components/FormLogin/index.jsx
@@ -1,34 +1,57 @@
 import React from 'react';
+// import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import ButtonForm from '../ButtonForm';
 import InputForm from '../InputForm';
 
 export default function FormLogin() {
+  const [redirect, setRedirect] = React.useState(false);
+
+  const handleClick = () => {
+    console.log(redirect);
+    setRedirect(true);
+  };
+
+  // const handleClick = () => <Redirect push to="/register" />;
+
   return (
     <form>
-      <InputForm
-        labelName="Login"
-        labelHtml="email"
-        id="email"
-        testId="common_login__input-email"
-        name="email"
-        type="email"
-      />
-      <InputForm
-        labelName="Password"
-        labelHtml="password"
-        id="password"
-        testId="common_login__input-password"
-        name="password"
-        type="password"
-      />
-      <ButtonForm
-        datatest="common_login__button-login"
-        name="Login"
-      />
-      <ButtonForm
-        datatest="common_login__button-register"
-        name="Ainda não tenho conta"
-      />
+      {
+        redirect ? <Redirect to="/register" /> : (
+          <>
+            <InputForm
+              labelName="Login"
+              labelHtml="email"
+              id="email"
+              testId="common_login__input-email"
+              name="email"
+              type="email"
+            />
+            <InputForm
+              labelName="Password"
+              labelHtml="password"
+              id="password"
+              testId="common_login__input-password"
+              name="password"
+              type="password"
+            />
+            <ButtonForm
+              datatest="common_login__button-login"
+              name="Login"
+            />
+            {/* Alteração feita porque o onCLick não funcionava no componente e a mesma falta em outros */}
+            <button
+              type="button"
+              datatest="common_login__button-register"
+              // name="Ainda não tenho conta"
+              onClick={ handleClick }
+            >
+              Ainda não tenho conta
+            </button>
+
+          </>
+        )
+      }
     </form>
   );
 }
