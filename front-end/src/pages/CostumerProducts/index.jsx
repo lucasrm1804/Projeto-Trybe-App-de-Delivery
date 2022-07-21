28ecaaf3eb64f9063126e007009c39e29eb54bd4
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-21 19:41:17 -0300

    chore: requisitos 11 ao 16 customer/products

--- front-end/src/pages/CostumerProducts/index.jsx
+++ front-end/src/pages/CostumerProducts/index.jsx
@@ -24,24 +24,6 @@ export default function CustomerProducts() {
     localStorage.setItem('carrinho', JSON.stringify([]));
   });
 
-  const [price, setPrice] = useState(0);
-
-  const totalPrice = () => {
-    const ls = JSON.parse(localStorage.getItem('carrinho'));
-    return ls.map((p) => Number(p.quantity) * Number(p.value))
-      .reduce((curr, acc) => {
-        console.log(curr);
-        acc += curr;
-        return acc;
-      }, 0);
-  };
-
-  useEffect(() => {
-    setPrice(totalPrice());
-  }, [setPrice]);
-
-  console.log(price);
-
   return (
     <>
       <Header />
@@ -57,7 +39,7 @@ export default function CustomerProducts() {
           </div>
         ))}
       </div>
-      <CartButton price={ price } />
+      <CartButton />
     </>
   );
 }
